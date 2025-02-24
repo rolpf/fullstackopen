0de3bd8cabd3088b084feff6113f0eef99c50547
rolpf <jiroxane@hotmail.fr>
2025-02-24 12:14:32 +0100

    part 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [names, setNames] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    console.log("button clibked", event.target);
+    const nameObject = {
+      name: newName,
+    };
+
+    setNames(names.concat(nameObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  );
+};
+
+export default App;
