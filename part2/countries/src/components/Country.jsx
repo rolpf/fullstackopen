a4a978706d57bfb3d14764a2e34ea0a2ebb17769
rolpf <jiroxane@hotmail.fr>
2025-04-13 12:27:14 +0200

    2.18 getting info about countries via API

---
+++ part2/countries/src/components/Country.jsx
@@ -0,0 +1,17 @@
+const Country = ({ country }) => {
+  const flagPng = country.flags.png;
+  console.log(country);
+  return (
+    <>
+      <h2>{country.name.common}</h2>
+      <ul>
+        <li>capital city : {country.capital}</li>
+        <li>population : {country.population}</li>
+      </ul>
+      <h3>Flag</h3>
+      <img src={country.flags.png} alt={country.flags.alt} width="100px"></img>
+    </>
+  );
+};
+
+export default Country;
---
+++ part2/countries/src/components/Results.jsx
@@ -0,0 +1,22 @@
+import Country from "./Country.jsx";
+
+const Results = ({ countries, setCountries }) => {
+  if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (
+    (countries.length > 2 && countries.length < 10) ||
+    countries.length === 0
+  ) {
+    return (
+      <ul>
+        {countries.map((country, i) => (
+          <li key={i}>{country.name.common}</li>
+        ))}
+      </ul>
+    );
+  } else {
+    return <Country country={countries[0]} />;
+  }
+};
+
+export default Results;
