ff7f90e2b3a7e62cc6ff7bdb153ed8e9c2795591
rolpf <82884248+rolpf@users.noreply.github.com>
2025-04-14 11:33:50 +0200

    Update Results.jsx

--- part2/countries/src/components/Results.jsx
+++ part2/countries/src/components/Results.jsx
@@ -3,12 +3,10 @@ import Country from "./Country.jsx";
 const Results = ({ countries, setCountries }) => {
   if (countries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
-  } else if (
-    (countries.length > 2 && countries.length < 10) ||
-    countries.length === 0
-  ) {
-    return (
-      <ul>
+  } 
+
+ return (
+     <ul>
         {countries.map((country, i) => (
           <li key={i}>
             {country.name.common}
@@ -17,9 +15,6 @@ const Results = ({ countries, setCountries }) => {
         ))}
       </ul>
     );
-  } else {
-    return <Country country={countries[0]} />;
-  }
 };
 
 export default Results;
