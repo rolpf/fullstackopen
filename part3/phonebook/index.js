0452c45b086fa0cbb40e5d395160fae2cf6d9ab4
rolpf <jiroxane@hotmail.fr>
2025-04-17 11:02:54 +0200

    part 3 : node js and express

---
+++ part3/phonebook/index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get("/info", (request, response) => {
+  const info = "Phonebook has info for " + persons.length + " people";
+  const date = new Date();
+  response.json(info + date);
+});
+
+const generateId = (max) => {
+  Math.floor(Math.random() * max);
+};
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({
+  //     error: "content  missing",
+  //   });
+  // }
+  //const person = {
+  //   name: body.name,
+  //   number: body.number,
+  //   id: generateId(1000),
+  // };
+  // persons = persons.concat(person);
+  console.log(person);
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
