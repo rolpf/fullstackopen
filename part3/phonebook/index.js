379439122e6f3262007769f7d7044bf4fee96a46
rolpf <jiroxane@hotmail.fr>
2025-05-06 11:26:58 +0200

    3.20 validation through mongoose

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -29,7 +29,7 @@ app.get("/api/info", (request, response) => {
   });
 });
 
-app.post("/api/persons", function (request, response) {
+app.post("/api/persons", function (request, response, next) {
   const body = request.body;
 
   const person = new Person({
@@ -37,37 +37,79 @@ app.post("/api/persons", function (request, response) {
     phone: body.phone,
   });
 
-  if (!body.name || !body.phone) {
-    return response.status(400).json({
-      error: "name or phone missing",
-    });
-  }
-
   Person.countDocuments({ name: body.name }).then((count) => {
     if (count > 0) {
       return response.status(409).json({
         error: "name must be unique",
       });
     } else {
-      person.save().then((savedPerson) => {
-        response.json(savedPerson);
-      });
+      person
+        .save()
+        .then((savedPerson) => {
+          response.json(savedPerson);
+        })
+        .catch((error) => next(error));
     }
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.patch("/api/persons/:id", (request, response, next) => {
+  const { phone } = request.body;
+
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end();
+      }
+
+      person.phone = phone;
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson);
+      });
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  Person.deleteOne({ _id: request.params.id }).then((person) => {
-    response.json(person);
-    response.status(204).end();
-  });
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+
+    next(error);
+  }
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, "0.0.0.0", () => {
--- part3/phonebook/src/models/person.js
+++ part3/phonebook/src/models/person.js
@@ -20,8 +20,23 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
+  name: {
+    type: String,
+    minLength: 3,
+    maxLength: 24,
+    required: true,
+  },
+  phone: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{2,6}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
