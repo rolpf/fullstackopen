f5b76848e85e95e82a247b1570b67507a3da0b65
rolpf.laptop <jiroxane@hotmail.fr>
2025-05-13 10:12:08 +0200

    tests backend

--- part4/bloginfo/src/tests/list_helper.test.js
+++ part4/bloginfo/src/tests/list_helper.test.js
@@ -11,42 +11,42 @@ const api = supertest(app);
 
 const blogs = [
   {
-    id: "5a422a851b54a676234d17f7",
+    id: "68223c4600f38814dea57e0c",
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
   },
   {
-    id: "5a422aa71b54a676234d17f8",
+    id: "68223c4600f38814dea57e0e",
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
   },
   {
-    id: "5a422b3a1b54a676234d17f9",
+    id: "68223c4600f38814dea57e10",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
   },
   {
-    id: "5a422b891b54a676234d17fa",
+    id: "68223c4600f38814dea57e12",
     title: "First class tests",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
   },
   {
-    id: "5a422ba71b54a676234d17fb",
+    id: "68223c4600f38814dea57e14",
     title: "TDD harms architecture",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
   },
   {
-    id: "5a422bc61b54a676234d17fc",
+    id: "68223c4600f38814dea57e16",
     title: "Type wars",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
@@ -56,23 +56,34 @@ const blogs = [
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = {};
-
-  for await (const blog of blogs) {
-    blogObject = new Blog(blog);
-    await blogObject.save();
-  }
+  await Blog.insertMany(helper.initialBlogs);
 });
 
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
-  assert.strictEqual(response.body.length, blogs.length);
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
 test("a specific blog is within the returned blogs", async () => {
-  const response = await api.get("/api/blogs");
-  const titles = response.body.map((e) => e.title);
-  assert(titles.includes("React patterns"), true);
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToView = blogsAtStart[0];
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  assert.deepStrictEqual(resultBlog.body, blogToView);
+});
+
+test("a blog can be deleted", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+  console.log(blogToDelete);
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+  const blogsAtEnd = await helper.blogsInDb();
+  const titles = blogsAtEnd.map((n) => n.title);
+  assert(!titles.includes(blogToDelete.title));
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
 });
 
 test("a valid blog can be added", async () => {
@@ -88,16 +99,14 @@ test("a valid blog can be added", async () => {
     .expect(201)
     .expect("Content-Type", /application\/json/);
 
-  const response = await api.get("/api/blogs");
+  const blogsAtEnd = await helper.blogsInDb();
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
 
-  const titles = response.body.map((r) => r.title);
-
-  assert.strictEqual(response.body.length, blogs.length + 1);
-
+  const titles = blogsAtEnd.map((n) => n.title);
   assert(titles.includes("async/await simplifies making async calls"));
 });
 
-test("a blog without a title is not added", async () => {
+test("blog without title is not added", async () => {
   const newBlog = {
     author: "Jose Brevet",
     url: "https://google.com",
@@ -105,9 +114,9 @@ test("a blog without a title is not added", async () => {
   };
   await api.post("/api/blogs").send(newBlog).expect(400);
 
-  const response = await api.get("/api/blogs");
+  const blogsAtEnd = await helper.blogsInDb();
 
-  assert.strictEqual(response.body.length, blogs.length);
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
 });
 
 test("dummy returns one", () => {
@@ -117,7 +126,6 @@ test("dummy returns one", () => {
 
 describe("total likes", () => {
   test("when list has only one blog, equals the likes of that", () => {
-    console.log(blogs[0]);
     const result = listHelper.totalLikes([blogs[0]]);
     assert.strictEqual(result, 7);
   });
@@ -180,8 +188,7 @@ describe("Author whose blog posts have the largest amount of likes", () => {
     });
   });
 });
--
-test.only("blogs are returned as json", async () => {
+-test.only("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
