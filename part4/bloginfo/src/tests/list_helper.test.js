6bd433048f707f489f90b488b2ead72eaf498504
rolpf.laptop <jiroxane@hotmail.fr>
2025-05-14 10:35:01 +0200

    part 4 : tests on backend

--- part4/bloginfo/src/tests/list_helper.test.js
+++ part4/bloginfo/src/tests/list_helper.test.js
@@ -59,31 +59,23 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 });
 
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
 test("a specific blog is within the returned blogs", async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToView = blogsAtStart[0];
-  const resultBlog = await api
-    .get(`/api/blogs/${blogToView.id}`)
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+  const response = await api.get("/api/blogs");
 
-  assert.deepStrictEqual(resultBlog.body, blogToView);
-});
-
-test("a blog can be deleted", async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToDelete = blogsAtStart[0];
-  console.log(blogToDelete);
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-  const blogsAtEnd = await helper.blogsInDb();
-  const titles = blogsAtEnd.map((n) => n.title);
-  assert(!titles.includes(blogToDelete.title));
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+  const titles = response.body.map((e) => e.title);
+  assert(titles.includes("React patterns"));
 });
 
 test("a valid blog can be added", async () => {
