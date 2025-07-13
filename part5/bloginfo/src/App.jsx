d8901b9d726026f92a4ff1679d08a2081a1a8265
rolpf <jiroxane@hotmail.fr>
2025-07-13 21:54:33 +0200

    5.1 conditional rendering if user, without calling component

--- part5/bloginfo/src/App.jsx
+++ part5/bloginfo/src/App.jsx
@@ -1,5 +1,120 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
 const App = () => {
-  return <>hello</>;
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setNotification("wrong credentials");
+      setTimeout(() => {
+        setNotification(null);
+      }, 5000);
+    }
+  };
+
+  // const loginForm = () => {
+  //   <div>
+  //     <h2>Login</h2>
+  //     <form onSubmit={handleLogin}>
+  //       <div>
+  //         username
+  //         <input
+  //           type="text"
+  //           value={username}
+  //           name="Username"
+  //           onChange={({ target }) => setUsername(target.value)}
+  //         />
+  //       </div>
+  //       <div>
+  //         password
+  //         <input
+  //           type="text"
+  //           value={password}
+  //           name="Password"
+  //           onChange={({ target }) => setPassword(target.value)}
+  //         />
+  //       </div>
+  //       <button type="submit">login</button>
+  //     </form>
+  //   </div>;
+  // };
+
+  useEffect(() => {
+    blogService.getAll().then((response) => {
+      setBlogs(response.data);
+    });
+  }, []);
+
+  // const blogList = () => {
+  //   <div>
+  //     <h2>blogs</h2>
+  //     {blogs.map((blog) => (
+  //       <Blog key={blog.id} blog={blog} />
+  //     ))}
+  //   </div>;
+  // };
+
+  // {
+  //   user === null && loginForm();
+  // }
+  // {
+  //   user !== null && blogList();
+  // }
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h2>blogs</h2>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div>
+              username
+              <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div>
+              password
+              <input
+                type="text"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
--- part5/bloginfo/src/main.jsx
+++ part5/bloginfo/src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
