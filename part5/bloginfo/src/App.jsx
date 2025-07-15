21844f569cd420777bc8752acbe843cadfe8fc1a
rolpf <jiroxane@hotmail.fr>
2025-07-15 09:18:46 +0200

    5.2 local storage login and logout

--- part5/bloginfo/src/App.jsx
+++ part5/bloginfo/src/App.jsx
@@ -10,13 +10,24 @@ const App = () => {
   const [user, setUser] = useState(null);
   const [notification, setNotification] = useState("");
 
-  //console.log("1 " + blogs);
   useEffect(() => {
     blogService.getAll().then((response) => {
       setBlogs(response);
     });
   }, []);
-  console.log("2 " + blogs);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
+  function logOut() {
+    window.localStorage.removeItem("loggedBlogappUser");
+    setUser("");
+  }
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -26,6 +37,7 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
@@ -85,6 +97,7 @@ const App = () => {
       {user ? (
         <div>
           connected user : {user.username}
+          <button onClick={logOut}>log out</button>
           <h2>blogs</h2>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
